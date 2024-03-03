222da7b8601ee97f38a49424af25a9a86d4f93d2
raanki <rabalone94@gmail.com>
2024-03-03 10:34:43 +0100

    norm

--- srcs/exe/render.c
+++ srcs/exe/render.c
@@ -6,7 +6,7 @@
 /*   By: ranki <ranki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 18:57:37 by ranki             #+#    #+#             */
-/*   Updated: 2024/03/03 10:25:29 by ranki            ###   ########.fr       */
+/*   Updated: 2024/03/03 10:33:53 by ranki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -192,6 +192,29 @@ void	hook_find_text_x_y(t_game *game)
 			/ 2 + game->lineHeight / 2) * game->step;
 }
 
+void	hook_draw_sprite(t_game *game, int x)
+{
+	int	y;
+	int	color;
+	int	texY;
+
+	y = game->drawStart - 1;
+	while (++y < game->drawEnd)
+	{
+		texY = (int)game->texPos & (TILE_SIZE - 1);
+		game->texPos += game->step;
+		color = game->sprite[(game->texNum)]->pixel_colors
+		[TILE_SIZE * texY + game->texX];
+		game->buffer[y][x] = color;
+	}
+	y = -1;
+	while (++y < game->drawStart)
+		game->buffer[y][x] = game->color_floor;
+	y = game->drawEnd - 1;
+	while (++y < SCREEN_HEIGHT)
+		game->buffer[y][x] = game->color_ceilling;
+}
+
 void	hook(t_game *game)
 {
 	int	x;
@@ -204,25 +227,9 @@ void	hook(t_game *game)
 		hook_find_hit(game);
 		hook_target_draw(game);
 		hook_find_text_x_y(game);
-		for(int y = game->drawStart; y < game->drawEnd; y++)
-		{
-			int texY = (int)game->texPos & (TILE_SIZE - 1);
-			game->texPos += game->step;
-			int color = game->sprite[(game->texNum)]->pixel_colors[TILE_SIZE * texY + game->texX];
-			game->buffer[y][x] = color;
-		}
-		for(int y = 0; y < game->drawStart; y++)
-		{
-			game->buffer[y][x] = game->color_floor;
-		}
-		for(int y = game->drawEnd; y < SCREEN_HEIGHT; y++)
-		{
-			game->buffer[y][x] = game->color_ceilling;
-		}
+		hook_draw_sprite(game, x);
 		x++;
 	}
-
-	//printf("exit loop\n");
 }
 
 int	game_loop(void)
@@ -231,15 +238,13 @@ int	game_loop(void)
 
 	game = game_instance();
 	mlx_destroy_image(game->mlx->mlx_p, game->mlx->img);
-	game->mlx->img = mlx_new_image(game->mlx->mlx_p, SCREEN_WIDTH, SCREEN_HEIGHT);
+	game->mlx->img = mlx_new_image(game->mlx->mlx_p,
+			SCREEN_WIDTH, SCREEN_HEIGHT);
 	game->mlx->addr = mlx_get_data_addr(game->mlx->img,
 			&game->mlx->bits_per_pixel, &game->mlx->line_length,
 			&game->mlx->endian);
-	for(int y = 0; y < SCREEN_HEIGHT; y++) for(int x = 0; x < SCREEN_WIDTH; x++) game->buffer[y][x] = 0;
 	hook(game);
 	draw_buffer(game);
-
-
 	mlx_put_image_to_window(game->mlx->mlx_p, game->mlx->win_p,
 		game->mlx->img, 0, 0);
 	return (0);
