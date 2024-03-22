9f83bfe88f085d7fac67a1e8db45476677f5b59d
raanki <rabalone94@gmail.com>
2024-03-22 18:53:36 +0100

    map close

--- srcs/parsing/flood_fill.c
+++ srcs/parsing/flood_fill.c
@@ -6,68 +6,97 @@
 /*   By: ranki <ranki@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 18:08:27 by ranki             #+#    #+#             */
-/*   Updated: 2024/03/22 18:49:36 by ranki            ###   ########.fr       */
+/*   Updated: 2024/03/22 18:53:05 by ranki            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../cub3d.h"
 
-int checkLines(char **map, int mapHeight, int mapWidth) {
-    for (int i = 0; i < mapHeight; i++) {
+int checkLines(char **map, int mapHeight, int mapWidth)
+{
+    int i = 0;
+    while (i < mapHeight)
+    {
         int foundOneFromLeft = 0;
-        int foundOneFromRight = 0;
-        for (int j = 0; j < mapWidth; j++) {
-            if (map[i][j] == '1') {
+        int j = 0;
+        while (j < mapWidth) {
+            if (map[i][j] == '1')
+            {
                 foundOneFromLeft = 1;
                 break;
             }
-            if (map[i][j] == '0' && !foundOneFromLeft) {
+            if (map[i][j] == '0' && !foundOneFromLeft)
+            {
                 return 0;
             }
+            j++;
         }
-        for (int j = mapWidth - 1; j >= 0; j--) {
-            if (map[i][j] == '1') {
+
+        int foundOneFromRight = 0;
+        j = mapWidth - 1;
+        while (j >= 0)
+        {
+            if (map[i][j] == '1')
+            {
                 foundOneFromRight = 1;
                 break;
             }
-            if (map[i][j] == '0' && !foundOneFromRight) {
+            if (map[i][j] == '0' && !foundOneFromRight)
+            {
                 return 0;
             }
+            j--;
         }
+        i++;
     }
     return 1;
 }
 
 
-int checkColumns(char **map, int mapHeight, int mapWidth) {
-    for (int j = 0; j < mapWidth; j++) {
+
+int checkColumns(char **map, int mapHeight, int mapWidth)
+{
+    int j = 0;
+    while (j < mapWidth)
+    {
         int foundOneFromTop = 0;
-        int foundOneFromBottom = 0;
-        for (int i = 0; i < mapHeight; i++) {
+        int i = 0;
+        while (i < mapHeight) {
             if (map[i][j] == '1') {
                 foundOneFromTop = 1;
                 break;
             }
-            if (map[i][j] == '0' && !foundOneFromTop) {
+            if (map[i][j] == '0' && !foundOneFromTop)
+            {
                 return 0;
             }
+            i++;
         }
-        for (int i = mapHeight - 1; i >= 0; i--) {
-            // De bas en haut
-            if (map[i][j] == '1') {
+
+        int foundOneFromBottom = 0;
+        i = mapHeight - 1;
+        while (i >= 0)
+        {
+            if (map[i][j] == '1')
+            {
                 foundOneFromBottom = 1;
                 break;
             }
-            if (map[i][j] == '0' && !foundOneFromBottom) {
+            if (map[i][j] == '0' && !foundOneFromBottom)
+            {
                 return 0;
             }
+            i--;
         }
+        j++;
     }
     return 1;
 }
 
 
-int mapValid(char **map, int mapHeight, int mapWidth) {
+
+int mapValid(char **map, int mapHeight, int mapWidth)
+{
     return checkLines(map, mapHeight, mapWidth) && checkColumns(map, mapHeight, mapWidth);
 }
 
